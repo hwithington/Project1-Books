278721fed08f44951b97ffaa45f20996fb00b167
hwithington <harrywithington@aol.co.uk>
2018-09-15 00:25:23 +0100

    homepage change for top rated.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # for postgres url
-DATABASE_URL = "postgres://gkaqijtewtmwny:e27d1431a1b316e308450efbb16ef2a52f87243f3078258292fdd6b87aeff426@ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/dbsd7p29t2igb1"
+DATABASE_URL = ""
 
 # Good read API Key
-BOOK_READ_API_KEY = "B6cHHPC1qz5feIfHopP6A"
+BOOK_READ_API_KEY = ""
 
 # Google Books API Key
-BOOK_API_KEY = "AIzaSyAqbgBlfSdKYiT3Z34cq5wTKl0aDEs6DkU"
+BOOK_API_KEY = ""
